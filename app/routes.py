6e20f8d092bcb675965c84e107b0dc6bd5b5c25d
Sosolidkk <pedrochaveslimas3@gmail.com>
2020-03-10 15:43:36 -0300

    Added sign up page

--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,17 @@ def index():
 
 @app.route("/sign-in")
 def sign_in():
-    flash("Error login", "danger")
-    flash("Success login", "success")
-    flash("Notification login", "primary")
+    flash("Error sign in", "danger")
+    flash("Success sign in", "success")
+    flash("Message sign in", "primary")
+
     return render_template("sign_in.html", title="Sign in page")
 
 
 @app.route("/sign-up")
 def sign_up():
+    flash("Error sign up", "danger")
+    flash("Success sign up", "success")
+    flash("Message sign up", "primary")
+
     return render_template("sign_up.html", title="Sign up page")
