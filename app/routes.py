8e157ef37e366a36bbfdf3d25d0828902c2995b6
Sosolidkk <pedrochaveslimas3@gmail.com>
2020-03-10 14:54:07 -0300

    Added sign in page

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,13 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html", title="Index page")
+
+
+@app.route("/sign-in")
+def sign_in():
+    return render_template("sign_in.html", title="Sign in page")
+
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("sign_up.html", title="Sign up page")
